5cc61964a7ed0290ba7962dbfcb233c5842844f5
Jeevan James <jeevanjj@live.com>
2018-12-16 03:06:02 +0530

    Initial commit.

---
+++ src/Collections/ByteCollectionExtensions.cs
@@ -0,0 +1,191 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace System.Collections.Generic
+{
+    /// <summary>
+    ///     Set of utility extension methods for byte collections.
+    /// </summary>
+    public static class ByteCollectionExtensions
+    {
+        /// <summary>
+        ///     Checks whether two byte collections are equal based on their content.
+        /// </summary>
+        /// <param name="source"> The first byte array to check. </param>
+        /// <param name="other"> The second byte array to check. </param>
+        /// <returns> True if the contents of the two byte arrays are equal. </returns>
+        public static bool IsEqualTo(this IList<byte> source, IList<byte> other)
+        {
+            if (ReferenceEquals(source, other))
+                return true;
+            if (source == null || other == null)
+                return false;
+            if (source.Count != other.Count)
+                return false;
+            for (int byteIdx = 0; byteIdx < source.Count; byteIdx++)
+            {
+                if (source[byteIdx] != other[byteIdx])
+                    return false;
+            }
+            return true;
+        }
+
+        public static bool IsEqualTo(this IList<byte> source, params byte[] other) =>
+            IsEqualTo(source, (IList<byte>) other);
+
+        /// <summary>
+        ///     Indicates whether the specified byte array is null or does not contain any elements.
+        /// </summary>
+        /// <param name="source"> The byte array to test. </param>
+        /// <returns> True if the byte array is either null or empty; other False. </returns>
+        public static bool IsNullOrEmpty(this IList<byte> source)
+        {
+            return source == null || source.Count == 0;
+        }
+
+        /// <summary>
+        ///     Indicates whether the specified byte array is null, does not contain any elements or consists
+        ///     of only zero value items.
+        /// </summary>
+        /// <param name="source"> The byte array to test. </param>
+        /// <returns> True if the byte array is null, does not contain any elements or consists exclusively of zero value items. </returns>
+        public static bool IsNullOrZeroed(this IList<byte> source)
+        {
+            if (source == null || source.Count == 0)
+                return true;
+            return source.All(b => b == 0);
+        }
+
+        /// <summary>
+        ///     Creates a string from a byte array that concatenates each item in the array, separated by
+        ///     the specified delimiter.
+        /// </summary>
+        /// <param name="source"> The byte array from which to create the string. </param>
+        /// <param name="delimiter"> The optional delimiter to separate each item in the array. Default to ',' if not specified. </param>
+        /// <returns> The combined string </returns>
+        public static string ToString(this IList<byte> source, string delimiter)
+        {
+            if (source == null)
+                return null;
+            if (source.Count == 0)
+                return string.Empty;
+
+            if (delimiter == null)
+                throw new ArgumentNullException(nameof(delimiter));
+            bool useDelimiter = delimiter.Length > 0;
+
+            var result = new StringBuilder(source[0].ToString(CultureInfo.InvariantCulture),
+                (source.Count * 3) + ((source.Count - 1) * delimiter.Length));
+            for (int i = 1; i < source.Count; i++)
+            {
+                if (useDelimiter)
+                    result.Append(delimiter);
+                result.Append(source[i]);
+            }
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// Retrieves the bytes from a byte array upto a specific sequence.
+        /// </summary>
+        /// <param name="source">The byte array</param>
+        /// <param name="start">The index in the array to start checking from</param>
+        /// <param name="sequence">The sequence to search for</param>
+        /// <returns>An array of bytes from the starting index to the matching sequence.</returns>
+        public static byte[] GetBytesUptoSequence(this byte[] source, int start, params byte[] sequence)
+        {
+            int sequenceIndex = IndexOfSequence(source, start, source.Length - start + 1, sequence);
+            if (sequenceIndex == -1)
+                return null;
+            var result = new byte[sequenceIndex - start];
+            Array.Copy(source, start, result, 0, result.Length);
+            return result;
+        }
+
+        public static int IndexOfSequence(this byte[] source, params byte[] sequence)
+        {
+            return IndexOfSequence(source, 0, source.Length, sequence);
+        }
+
+        public static int IndexOfSequence(this byte[] source, int start, int count, byte[] sequence)
+        {
+            var sequenceIndex = 0;
+            int endIndex = Math.Min(source.Length, start + count);
+            for (int byteIdx = start; byteIdx < endIndex; byteIdx++)
+            {
+                if (source[byteIdx] == sequence[sequenceIndex])
+                {
+                    sequenceIndex++;
+                    if (sequenceIndex >= sequence.Length)
+                        return byteIdx - sequence.Length + 1;
+                }
+                else
+                    sequenceIndex = 0;
+            }
+            return -1;
+        }
+
+        public static int[] IndexOfSequences(this byte[] source, params byte[] sequence)
+        {
+            return IndexOfSequences(source, 0, source.Length, sequence);
+        }
+
+        public static int[] IndexOfSequences(this byte[] source, int start, int count, byte[] sequence)
+        {
+            var locations = new List<int>();
+
+            int sequenceLocation = IndexOfSequence(source, start, count, sequence);
+            while (sequenceLocation >= 0)
+            {
+                locations.Add(sequenceLocation);
+                count -= sequenceLocation - start + 1;
+                start = sequenceLocation + sequence.Length;
+                sequenceLocation = IndexOfSequence(source, start, count, sequence);
+            }
+
+            return locations.ToArray();
+        }
+
+        public static byte[][] SplitBySequence(this byte[] source, params byte[] sequence)
+        {
+            return SplitBySequence(source, 0, source.Length, sequence);
+        }
+
+        public static byte[][] SplitBySequence(this byte[] source, int start, int count, byte[] sequence)
+        {
+            if (start + count > source.Length)
+                count = source.Length - start;
+
+            int[] locations = IndexOfSequences(source, start, count, sequence);
+            if (locations.Length == 0)
+                return new[] { source };
+
+            var results = new List<byte[]>(locations.Length + 1);
+            for (int locationIdx = 0; locationIdx < locations.Length; locationIdx++)
+            {
+                int startIndex = locationIdx > 0 ? locations[locationIdx - 1] + sequence.Length : start;
+                int endIndex = locations[locationIdx] - 1;
+                if (endIndex < startIndex)
+                    results.Add(new byte[0]);
+                else
+                {
+                    var splitBytes = new byte[endIndex - startIndex + 1];
+                    Array.Copy(source, startIndex, splitBytes, 0, splitBytes.Length);
+                    results.Add(splitBytes);
+                }
+            }
+
+            if (locations[locations.Length - 1] + sequence.Length > start + count - 1)
+                results.Add(new byte[0]);
+            else
+            {
+                var splitBytes = new byte[start + count - locations[locations.Length - 1] - sequence.Length];
+                Array.Copy(source, locations[locations.Length - 1] + sequence.Length, splitBytes, 0, splitBytes.Length);
+                results.Add(splitBytes);
+            }
+
+            return results.ToArray();
+        }
+    }
+}
---
+++ src/Collections/CollectionExtensions.cs
@@ -0,0 +1,285 @@
+﻿using System.Linq;
+
+namespace System.Collections.Generic
+{
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        ///     Adds the elements of the specified <see cref="IEnumerable{T}" /> to the collection
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of the collection</typeparam>
+        /// <param name="collection">The collection to add the items to.</param>
+        /// <param name="items">
+        ///     The <see cref="IEnumerable{T}" /> whose elements should be added to the end of the collection. The
+        ///     <see cref="IEnumerable{T}" /> itself cannot be null, but it can contain elements that are null, if type T is a
+        ///     reference type.
+        /// </param>
+        /// <exception cref="ArgumentNullException">collection is null</exception>
+        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items)
+        {
+            if (collection == null)
+                throw new ArgumentNullException("collection");
+            if (items == null)
+                return;
+
+            foreach (T item in items)
+                collection.Add(item);
+        }
+
+        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items, Func<T, bool> predicate)
+        {
+            if (collection == null)
+                throw new ArgumentNullException("collection");
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+            if (items == null)
+                return;
+
+            collection.AddRange(items.Where(predicate));
+        }
+
+        public static void AddRange<TDest, TSource>(this ICollection<TDest> collection, IEnumerable<TSource> items,
+            Func<TSource, TDest> converter)
+        {
+            if (collection == null)
+                throw new ArgumentNullException("collection");
+            if (converter == null)
+                throw new ArgumentNullException("converter");
+            if (items == null)
+                return;
+
+            collection.AddRange(items.Select(item => converter(item)));
+        }
+
+        public static void AddRange<TDest, TSource>(this ICollection<TDest> collection, IEnumerable<TSource> items,
+            Func<TSource, bool> predicate, Func<TSource, TDest> converter)
+        {
+            if (collection == null)
+                throw new ArgumentNullException("collection");
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+            if (converter == null)
+                throw new ArgumentNullException("converter");
+            if (items == null)
+                return;
+
+            collection.AddRange(items.Where(predicate).Select(item => converter(item)));
+        }
+
+        /// <summary>
+        ///     Populates each item in a byte collection with a specific value.
+        /// </summary>
+        /// <param name="source"> The byte array to be populated. </param>
+        /// <param name="value"> The value to populate the byte array with. </param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static void Fill<T>(this IList<T> source, T value)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (source.Count == 0)
+                return;
+            for (int i = 0; i < source.Count; i++)
+                source[i] = value;
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            if (source == null)
+                return;
+            if (action == null)
+                throw new ArgumentNullException("action");
+
+            foreach (T item in source)
+                action(item);
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T, int> action)
+        {
+            if (source == null)
+                return;
+            if (action == null)
+                throw new ArgumentNullException("action");
+
+            var index = 0;
+            foreach (T item in source)
+                action(item, index++);
+        }
+
+        public static int IndexOf<T>(this IList<T> list, Func<T, bool> predicate)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (predicate(list[i]))
+                    return i;
+            }
+            return -1;
+        }
+
+        public static IEnumerable<int> IndexOfAll<T>(this IList<T> list, Func<T, bool> predicate)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (predicate(list[i]))
+                    yield return i;
+            }
+        }
+
+        public static bool IsEmpty<T>(this IEnumerable<T> source) =>
+            source is ICollection<T> collection ? collection.Count == 0 : !source.Any();
+
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+                return true;
+            return source is ICollection<T> collection ? collection.Count == 0 : !source.Any();
+        }
+
+        public static int LastIndexOf<T>(this IList<T> list, Func<T, bool> predicate)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                if (predicate(list[i]))
+                    return i;
+            }
+            return -1;
+        }
+
+        /// <summary>
+        ///     Determines whether none of the elements of a sequence satisfies the specified condition
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}" /> whose elements to apply the predicate to.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <returns>true if any elements in the source sequence pass the test in the specified predicate; otherwise, false.</returns>
+        /// <exception cref="ArgumentNullException">source or predicate is null</exception>
+        public static bool None<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+            return !source.Any(predicate);
+        }
+
+        public static bool Remove<T>(this IList<T> list, Func<T, bool> predicate)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (predicate(list[i]))
+                {
+                    list.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static int RemoveAll<T>(this IList<T> list, Func<T, bool> predicate)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+
+            int count = 0;
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                if (predicate(list[i]))
+                {
+                    list.RemoveAt(i);
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static bool RemoveLast<T>(this IList<T> list, Func<T, bool> predicate)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                if (predicate(list[i]))
+                {
+                    list.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static IEnumerable<T> Repeat<T>(this IEnumerable<T> source, int count)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (count < 0)
+                throw new ArgumentOutOfRangeException("count");
+            for (var i = 0; i < count; i++)
+            {
+                foreach (T item in source)
+                    yield return item;
+            }
+        }
+
+        public static TOutput[] ToArray<TInput, TOutput>(this IEnumerable<TInput> source,
+            Func<TInput, TOutput> converter)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (converter == null)
+                throw new ArgumentNullException("converter");
+
+            var array = new TOutput[source.Count()];
+            var counter = 0;
+            foreach (TInput item in source)
+                array[counter++] = converter(item);
+            return array;
+        }
+
+        public static T[] ToArray<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+
+            return source.Where(predicate).ToArray();
+        }
+
+        public static TOutput[] ToArray<TInput, TOutput>(this IEnumerable<TInput> source, Func<TInput, bool> predicate,
+            Func<TInput, TOutput> converter)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+            if (converter == null)
+                throw new ArgumentNullException("converter");
+
+            return source.Where(predicate).Select(item => converter(item)).ToArray();
+        }
+    }
+}
