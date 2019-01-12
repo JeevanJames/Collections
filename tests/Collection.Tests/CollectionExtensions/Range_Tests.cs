67060d624665b2b746e503b75f1d47c17bd7056c
Jeevan James <jeevanjj@live.com>
2019-01-13 00:23:52 +0530

    Fixed Range unit test.

--- tests/Collection.Tests/CollectionExtensions/Range_Tests.cs
+++ tests/Collection.Tests/CollectionExtensions/Range_Tests.cs
@@ -45,17 +45,18 @@ namespace Collection.Tests.CollectionExtensions
         }
 
         [Theory, DataAttributes.Collection(CollectionType.NonEmpty)]
-        public void Throws_if_end_is_less_than_start_or_out_of_range(ICollection<int> collection)
+        public void Throws_if_end_is_less_than_start(ICollection<int> collection)
         {
             Should.Throw<ArgumentOutOfRangeException>(() => collection.Range(2, 1));
-            Should.Throw<ArgumentOutOfRangeException>(() => collection.Range(2, collection.Count));
         }
 
         [Theory]
         [InlineData(new[] {1, 2, 3, 4, 5, 6}, 2, 4, new[] {3, 4})]
         [InlineData(new[] {1, 2, 3, 4, 5, 6}, null, 3, new[] {1, 2, 3})]
         [InlineData(new[] {1, 2, 3, 4, 5, 6}, 3, null, new[] {4, 5, 6})]
+        [InlineData(new[] {1, 2, 3, 4, 5, 6}, 3, 200, new[] {4, 5, 6})]
         [InlineData(new[] {1, 2, 3, 4, 5, 6}, null, null, new[] {1, 2, 3, 4, 5, 6})]
+        [InlineData(new[] {1, 2, 3, 4, 5, 6}, null, 100, new[] {1, 2, 3, 4, 5, 6})]
         [InlineData(new[] {1, 2, 3, 4, 5, 6}, 3, 3, new int[0])]
         public void Returns_iterator_for_specified_range(ICollection<int> collection, int? start, int? end, IList<int> expectedResult)
         {
