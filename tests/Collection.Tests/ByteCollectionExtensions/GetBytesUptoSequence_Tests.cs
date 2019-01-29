b4a1708f1490ac22ee512c08759e35a6de3c84b3
Jeevan James <jeevanjj@live.com>
2019-01-29 12:25:44 +0530

    Added int collection tests.

--- tests/Collection.Tests/ByteCollectionExtensions/GetBytesUptoSequence_Tests.cs
+++ tests/Collection.Tests/ByteCollectionExtensions/GetBytesUptoSequence_Tests.cs
@@ -32,7 +32,7 @@ namespace Collection.Tests.ByteCollectionExtensions
         [InlineData(new byte[] {1, 2, 3, 4, 5, 6}, new byte[] {1, 2, 3}, 0, new byte[0])]
         public void Returns_bytes_upto_sequence(byte[] bytes, byte[] sequence, int start, byte[] expectedResult)
         {
-            IList<byte> result = bytes.GetBytesUptoSequence(start, sequence);
+            IList<byte> result = bytes.GetNumbersUptoSequence(start, sequence);
 
             result.ShouldBe(expectedResult);
         }
@@ -43,7 +43,7 @@ namespace Collection.Tests.ByteCollectionExtensions
         [InlineData(new byte[] {1, 2, 3, 4, 5, 6}, new byte[] {1, 2, 3}, 1)]
         public void Returns_null_if_sequence_not_found(byte[] bytes, byte[] sequence, int start)
         {
-            IList<byte> result = bytes.GetBytesUptoSequence(start, sequence);
+            IList<byte> result = bytes.GetNumbersUptoSequence(start, sequence);
 
             result.ShouldBeNull();
         }
